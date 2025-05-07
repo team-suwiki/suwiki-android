e67327a52cd6a725e2a3603bbe0affc6702ce124
jinukeu <koownij@kakao.com>
2025-05-08 01:22:33 +0900

    feat/#168: setup Design System for iOS
    - Added AppColor enum for common colors.
    - Added color asset files for various shades of blue, green, orange, and gray, as well as black, white, primary, and error colors.
    - Configured the project to include color assets.
    - Added Typography struct and TextStyle struct for managing text styles.
    - Added TypographyModifier view modifier for applying text styles.
    - Added a View extension for easy application of typography styles.
    - Added NotoSansKR font files (Bold, Light, Medium, Regular).
    - Updated ContentView to use the new Typography and AppColor.
    - Configured project settings to include the new font files and exclude Info.plist from file synchronization for the "suwiki" folder.

---
+++ iosApp/suwiki/Presentation/Common/Theme/TypographyTheme.swift
@@ -0,0 +1,34 @@
+//
+//  TypographyTheme.swift
+//  suwiki
+//
+//  Created by 이진욱 on 5/8/25.
+//
+import SwiftUI
+
+final class TypographyTheme: ObservableObject {
+
+  let header1 = Typography.header1
+  let header2 = Typography.header2
+  let header3 = Typography.header3
+  let header4 = Typography.header4
+  let header5 = Typography.header5
+  let header6 = Typography.header6
+  let header7 = Typography.header7
+  
+  let body1 = Typography.body1
+  let body2 = Typography.body2
+  let body3 = Typography.body3
+  let body4 = Typography.body4
+  let body5 = Typography.body5
+  let body6 = Typography.body6
+  let body7 = Typography.body7
+
+  let caption1 = Typography.caption1
+  let caption2 = Typography.caption2
+  let caption3 = Typography.caption3
+  let caption4 = Typography.caption4
+  let caption5 = Typography.caption5
+  let caption6 = Typography.caption6
+  let caption7 = Typography.caption7
+}
--- iosApp/suwiki/suwikiApp.swift
+++ iosApp/suwiki/suwikiApp.swift
@@ -12,6 +12,7 @@ struct suwikiApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+            .environmentObject(TypographyTheme())
         }
     }
 }
